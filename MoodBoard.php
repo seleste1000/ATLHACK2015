f101f3d2390b5012c5e945d091413c037709329b
jsnell1995 <jsnell1995@gmail.com>
2015-10-17 21:49:56 -0400

    Update MoodBoard.php

--- MoodBoard.php
+++ MoodBoard.php
@@ -18,13 +18,13 @@
     <a href='#' class='button'>Happy</a>
     .button {
    border: 1px solid #0a3c59;
-   background: url ('/image/btn.png') no-repeat;
-   background: -webkit-gradient(linear, left top, left bottom, from(#65a9d7), to(#3e779d));
-   background: -webkit-linear-gradient(top, #65a9d7, #3e779d);
-   background: -moz-linear-gradient(top, #65a9d7, #3e779d);
-   background: -ms-linear-gradient(top, #65a9d7, #3e779d);
-   background: -o-linear-gradient(top, #65a9d7, #3e779d);
-   background-image: -ms-linear-gradient(top, #65a9d7 0%, #3e779d 100%);
+   background: #000000;
+   background: -webkit-gradient(linear, left top, left bottom, from(#65a9d7), to(#000000));
+   background: -webkit-linear-gradient(top, #65a9d7, #000000);
+   background: -moz-linear-gradient(top, #65a9d7, #000000);
+   background: -ms-linear-gradient(top, #65a9d7, #000000);
+   background: -o-linear-gradient(top, #65a9d7, #000000);
+   background-image: -ms-linear-gradient(top, #65a9d7 0%, #000000 100%);
    padding: 20px 40px;
    -webkit-border-radius: 0px;
    -moz-border-radius: 0px;
