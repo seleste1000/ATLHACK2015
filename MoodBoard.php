282fa96c9f21e04f2e700f216e0c2aa8c10edfdf
KevinWomack <milanwomack@live.com>
2015-10-17 20:29:06 -0400

    Update MoodBoard.php

--- MoodBoard.php
+++ MoodBoard.php
@@ -10,12 +10,58 @@
 
 <body>
     
-    //each mood gets a button
+    <table>
+    <tr>
+    
+    <td>
+        //each mood gets a button <!--Happy-->
     <FORM METHOD="LINK" ACTION="page1.htm">
     <INPUT TYPE="submit" VALUE="Clickable Button">
     </FORM>
     
+    </td>
     
+    <td>
+      <!--Excited button goes here-->  
+    </td>
+    
+    <td>
+      <!--Loved button goes here-->  
+    </td>
+    
+    <td>
+       <!--Confident button goes here--> 
+    </td>
+    </tr><!--New row begins on next line-->
+    <tr>
+        <td>
+            <!--Relaxed button goes here-->
+        </td>  
+        <td>
+            <!--Angry button goes here-->
+        </td>
+        <td>
+            <!--Scared button goes here-->
+        </td>
+        <td>
+          <!--Sad button goes here-->  
+        </td>
+    </tr><!--New row begins on next line-->
+    <tr>
+            <td>
+            <!--Stressed button goes here-->    
+            </td>
+            <td>
+            <!--Lonely button goes here-->    
+            </td>
+            <td>
+            <!--Confused button goes here-->
+            </td>
+            <td>
+            <!--Neutral button goes here-->    
+            </td>
+    </tr>
+    </table><!--Table ends-->
 </body>
 
 
